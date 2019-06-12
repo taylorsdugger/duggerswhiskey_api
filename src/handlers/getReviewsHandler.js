c68f2e3e63267fbd541e57423cd7313cd1650316
taylorsdugger <taylorsdugger@gmail.com>
2019-06-11 19:49:24 -0500

    avg not sum

--- src/handlers/getReviewsHandler.js
+++ src/handlers/getReviewsHandler.js
@@ -4,11 +4,12 @@ let reviewModel = require('../models/review');
 
 async function getReviews(queryParams) {
   const reviews = await reviewModel.find({}, null, { skip: (--queryParams.page * queryParams.limit), limit: queryParams.limit });
-  const reviewSum = await reviewModel.aggregate().group({_id: "$rating", total: { $sum: "$rating"}});
+  const reviewSum = await reviewModel.aggregate().group({_id: null, sum: { $sum: "$rating"}});
+  const reviewCount = await reviewModel.count("rating");
 
   return {
     reviews,
-    reviewSum
+    overallRating: reviewSum[0].sum/reviewCount
   }
 }
 
